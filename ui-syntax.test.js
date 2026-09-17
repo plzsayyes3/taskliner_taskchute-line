0958398de0e3832d3474d74cc708dd7775129064
plzsayyes3 <102906997+plzsayyes3@users.noreply.github.com>
2026-09-17 20:10:07 +0900

    Add UI inline script syntax checks

---
+++ ui-syntax.test.js
@@ -0,0 +1,12 @@
+const test=require('node:test');
+const assert=require('node:assert/strict');
+const fs=require('node:fs');
+const files=['master.html','repeat.html','today.html','index-v2.html','taskliner-v2.html'];
+for(const file of files){
+  test(`${file} inline scripts parse`,()=>{
+    const html=fs.readFileSync(file,'utf8');
+    const scripts=[...html.matchAll(/<script(?![^>]*\btype=["']text\/plain["'])[^>]*>([\s\S]*?)<\/script>/gi)].map(m=>m[1]).filter(Boolean);
+    assert.ok(scripts.length>0,`${file} should contain inline script`);
+    for(const source of scripts)assert.doesNotThrow(()=>new Function(source),`${file} contains invalid JavaScript`);
+  });
+}
